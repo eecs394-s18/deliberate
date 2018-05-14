f0c193f1c0e0a0b6a1239505bb79b315c778b96c
Yaoyuchi <yuchiyao2019@u.northwestern.edu>
2018-05-14 17:29:16 -0500

    Click on margin or on card

--- src/Card.js
+++ src/Card.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 import './Card.css';
-
+/**
+ * @param x: The x-coordinate on this page 
+ * @param y: The y-coordinate on this page 
+ * @param xSet : The offset on X from right -> left on each card
+ * @param ySet : The offset on Y from right -> left on each card
+ */
 class Card extends Component {
   static isClicked = false;
   constructor(props){
     super(props);
-    this.state = {x:0,y:0};
+    this.state = {x:0,y:0,xSet:0,ySet:0,cardWidth:0,cardHeight:0,margin:4};
   }
-  connectLine(){
-    console.log(this.state.x +" "+this.state.y );
+  clickOnCard(){
+    console.log(this.state.x +" "+this.state.y + " "+this.state.xSet +" "+this.state.ySet + 
+                " " + this.state.cardHeight + " " + this.state.cardWidth);
+    // the mouse on the margin
+    if(this.state.xSet< this.state.margin || this.state.xSet > this.state.cardHeight - this.state.margin || 
+       this.state.ySet < this.state.margin || this.state.ySet > this.state.cardHeight- this.state.margin){
+      this.clickOnMargin();
+    }else{
+      console.log("Not on the card");
+    }
+  }
+  clickOnMargin(){
+    console.log("On the margin");
   }
   _onMouseMove(e){
-    this.setState({ x: e.screenX, y: e.screenY });
+    var bounds = e.target.getBoundingClientRect();
+    var offsetX = e.clientX - bounds.left;
+    var offsetY = e.clientY - bounds.top;
+    var width = bounds.right - bounds.left;
+    var height =  bounds.bottom - bounds.top;
+    this.setState({ x: e.screenX, y: e.screenY, xSet:offsetX,ySet:offsetY,cardWidth:width,cardHeight:height});
   }
   render() {
     return (
-      <div className="cardBorder" ref="element" onClick={this.connectLine.bind(this)} 
+      <div className="cardBorder"  onClick={this.clickOnCard.bind(this)} 
         onMouseMove={this._onMouseMove.bind(this)}> 
-        <div className="Card">{this.props.cardId}</div>
+        <div className="Card" >{this.props.cardId}</div>
       </div>
       
     );
