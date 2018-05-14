2e2a4453a2c5cd263881bb56fc4d1db6205d986d
Aaron Kamel <aaronkamel2019@u.northwestern.edu>
2018-05-14 16:08:14 -0500

    minor import adds

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './Card.css';
+import GridSection from './GridSection.js';
+import CONSTANTS from './Constants.js';
+import Grid from './Grid.js'
 
 class Card extends Component {
   render() {
     return (
       <div className="Card">
-        {this.props.cardId}
             <img 
                 className="deleteButton" 
-                src={require('./icons/iconmonstr-x-mark-4.svg')}
+                src={require('./icons/iconmonstr-x-mark.svg')}
                 onClick={this.props.deletefromList}
                 alt="Delete Button"
             />
+            {this.props.cardId}
       </div>
 
     );
